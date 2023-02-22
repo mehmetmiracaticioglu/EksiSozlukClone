d64d055385813f24df12c025ceed1344120a0799
mehmet miraç atıcıoğlu <mehmet_mirac_1@hotmail.com>
2023-02-22 13:02:12 +0300

    domain entityler tamamlandı

---
+++ src/Api/Core/EksiSozlukClone.Domain/Models/EntryCommentVote.cs
@@ -0,0 +1,19 @@
+﻿using EksiSozlukClone.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksiSozlukClone.Core.Domain.Models;
+
+public class EntryCommentVote : BaseEntity
+{
+    public Guid EntryCommentId { get; set; }   
+    public Guid CreatedById { get; set; }   
+    public VoteType voteType { get; set; }  
+
+    public virtual EntryComment EntryComment { get; set; }
+
+
+}
---
+++ src/Api/Core/EksiSozlukClone.Domain/Models/EntryVote.cs
@@ -0,0 +1,19 @@
+﻿using EksiSozlukClone.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksiSozlukClone.Core.Domain.Models;
+
+public class EntryVote:BaseEntity
+{
+    public Guid EntryId { get; set; }   
+    public Guid CreatedById { get; set; }   
+    public VoteType voteType { get; set; }  
+
+    public virtual Entry Entry { get; set; }
+
+
+}
