10c7638ded73004f432d311415420fe1d65bc86e
mehmet miraç atıcıoğlu <mehmet_mirac_1@hotmail.com>
2023-02-22 12:29:57 +0300

    Add project files.

---
+++ src/Projections/EksiSozlukClone.Projections.FavoriteService/Program.cs
@@ -0,0 +1,10 @@
+using EksiSozlukClone.Projections.FavoriteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
